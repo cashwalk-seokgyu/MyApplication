c9d5f8d013a71711e053e25d1f93c5c8dc56ff48
이석규 <lee.seokgyu@cashwalk.io>
2024-02-06 23:35:02 +0900

    어이쿠 커밋을 잘못했네 3

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -24,6 +24,7 @@ class MainActivity : ComponentActivity() {
                     Greeting("Test")
                     Greeting("Test")
                     Greeting("Test")
+                    Greeting("Test")
                 }
             }
         }
